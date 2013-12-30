30cd4ee543b7930975ff76ca87bbaf4fba4c6599
apple <apple@10.137.1.117>
2013-12-30 20:44:57 +0800

    c

--- test/src/main/java/web/SystemController.java
+++ test/src/main/java/web/SystemController.java
@@ -4,14 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
-public class SystemController implements Controller {
-	public void registe(String username, String password) {
-		
+public class SystemController extends MultiActionController {
+	
+	public ModelAndView registe(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("index");
 	}
 
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public ModelAndView login() {
 		System.out.println("testController");
 		return new ModelAndView("login");
 	}
