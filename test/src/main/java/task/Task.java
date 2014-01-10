08e6189235ba6336b5c6b1610393ac4def1d51c5
jiangyu <jiangyu@cupboys.cn>
2014-01-10 15:09:46 +0800

    spring test project

---
+++ test/src/main/java/task/Task.java
@@ -0,0 +1,23 @@
+package task;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Task {
+	public void task() {
+		Timer timer = new Timer();
+		timer.schedule(new MyTask(), 5000, 1000);
+		while (true) {
+			
+		}
+	}
+	
+	private final class MyTask extends TimerTask {
+
+		@Override
+		public void run() {
+			System.out.println("test");
+		}
+		
+	}
+}
---
+++ test/src/test/java/task/TaskTest.java
@@ -0,0 +1,18 @@
+package task;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskTest {
+	private Task task = new Task();
+	
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTask() {
+		task.task();
+	}
+}
