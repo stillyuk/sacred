6a6917befca42c8319c994b3f39998cd361da94c
jiangyu <jiangyu@cupboys.cn>
2014-01-12 11:09:52 +0800

    init struts2 project

--- spring/src/main/java/mail/SimpleMailSender.java
+++ spring/src/main/java/mail/SimpleMailSender.java
@@ -17,35 +17,22 @@ import javax.mail.internet.MimeMultipart;
 
 public class SimpleMailSender {
 	public static boolean sendTextMail(MailSenderInfo mailInfo) {
-		// 判断是否需要身份认证
 		MyAuthenticator authenticator = null;
 		Properties pro = mailInfo.getProperties();
 		if (mailInfo.isValidate()) {
-			// 如果需要身份认证，则创建一个密码验证器
-			authenticator = new MyAuthenticator(mailInfo.getUserName(),
-					mailInfo.getPassword());
+			authenticator = new MyAuthenticator(mailInfo.getUserName(), mailInfo.getPassword());
 		}
-		// 根据邮件会话属性和密码验证器构造一个发送邮件的session
-		Session sendMailSession = Session
-				.getDefaultInstance(pro, authenticator);
+		Session sendMailSession = Session.getDefaultInstance(pro, authenticator);
 		try {
-			// 根据session创建一个邮件消息
 			Message mailMessage = new MimeMessage(sendMailSession);
-			// 创建邮件发送者地址
 			Address from = new InternetAddress(mailInfo.getFromAddress());
-			// 设置邮件消息的发送者
 			mailMessage.setFrom(from);
-			// 创建邮件的接收者地址，并设置到邮件消息中
 			Address to = new InternetAddress(mailInfo.getToAddress());
 			mailMessage.setRecipient(Message.RecipientType.TO, to);
-			// 设置邮件消息的主题
 			mailMessage.setSubject(mailInfo.getSubject());
-			// 设置邮件消息发送的时间
 			mailMessage.setSentDate(new Date());
-			// 设置邮件消息的主要内容
 			String mailContent = mailInfo.getContent();
 			mailMessage.setText(mailContent);
-			// 发送邮件
 			Transport.send(mailMessage);
 			return true;
 		} catch (MessagingException ex) {
@@ -60,12 +47,10 @@ public class SimpleMailSender {
 		Properties pro = mailInfo.getProperties();
 		// 如果需要身份认证，则创建一个密码验证器
 		if (mailInfo.isValidate()) {
-			authenticator = new MyAuthenticator(mailInfo.getUserName(),
-					mailInfo.getPassword());
+			authenticator = new MyAuthenticator(mailInfo.getUserName(), mailInfo.getPassword());
 		}
 		// 根据邮件会话属性和密码验证器构造一个发送邮件的session
-		Session sendMailSession = Session
-				.getDefaultInstance(pro, authenticator);
+		Session sendMailSession = Session.getDefaultInstance(pro, authenticator);
 		try {
 			// 根据session创建一个邮件消息
 			Message mailMessage = new MimeMessage(sendMailSession);
--- spring/src/test/java/mail/MyJavaMailTest.java
+++ spring/src/test/java/mail/MyJavaMailTest.java
@@ -1,10 +1,24 @@
 package mail;
 
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class MyJavaMailTest {
 
-	@Test
 	public void test() {
 		MailSenderInfo mailInfo = new MailSenderInfo();
 		mailInfo.setMailServerHost("smtp.163.com");
@@ -18,6 +32,41 @@ public class MyJavaMailTest {
 		mailInfo.setContent("content");
 
 		SimpleMailSender.sendTextMail(mailInfo);
-		SimpleMailSender.sendHtmlMail(mailInfo);
+	}
+
+	public void testSend() throws Exception {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.163.com");
+		props.put("mail.smtp.port", "25");
+		props.put("mail.smtp.auth", "true");
+		Authenticator authenticator = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("x31001333@163.com", "tkggycb");
+			}
+		};
+		Session session = Session.getDefaultInstance(props, authenticator);
+		Message message = new MimeMessage(session);
+		Address to = new InternetAddress("191295604@qq.com");
+		message.setFrom(new InternetAddress("18768105877@163.com"));
+		message.setRecipient(Message.RecipientType.TO, to);
+		message.setSubject("TEST...");
+		message.setText("no reply");
+		Transport.send(message);
+	}
+
+	@Test
+	public void testSpringMail() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("springMail.xml");
+		JavaMailSender sender = (JavaMailSender) ctx.getBean("springMail");
+		SimpleMailMessage mail = new SimpleMailMessage();
+		try {
+			mail.setTo("191295604@qq.com");
+			mail.setFrom("18768105877@163.com");
+			mail.setSubject("spring mail test!");
+			mail.setText("springMail的简单发送测试");
+			sender.send(mail);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
