6f7e0f0316da5aa27cfbb06725c01408ee386c3c
jiangyu <jiangyu@cupboys.cn>
2014-01-14 13:46:21 +0800

    2014-1-14

--- test/src/main/java/wsyumi/log4j/MyLog4j.java
+++ test/src/main/java/wsyumi/log4j/MyLog4j.java
@@ -1,12 +1,11 @@
-package log4j;
+package wsyumi.log4j;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
 public class MyLog4j {
 	public static void main(String[] args) {
-		Logger logger = LogManager.getLogger();
+		Logger logger = LogManager.getLogger("ww");
+		logger.warn("aa");
 	}
 }
