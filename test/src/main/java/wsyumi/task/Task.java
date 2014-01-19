3c49fd15fecc2d4a2f9235f5207a5584dccb2a5b
jiangyu <jiangyu@cupboys.cn>
2014-01-19 17:33:07 +0800

    c

--- test/src/main/java/wsyumi/task/Task.java
+++ test/src/main/java/wsyumi/task/Task.java
@@ -6,7 +6,7 @@ import java.util.TimerTask;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Task {
+public class Task implements ITask {
 	public void task() {
 		Timer timer = new Timer();
 		timer.schedule(new MyTask(), 5000, 1000);
